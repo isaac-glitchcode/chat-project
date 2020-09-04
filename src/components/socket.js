8b72801bed9cf7717c05261057d2b156f39de0cd
isaac-glitchcode <54855963+isaac-glitchcode@users.noreply.github.com>
2020-09-03 23:19:13 -0500

    Update socket.js

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,6 +1,5 @@
 import io from 'socket.io-client';
 
-let socket = io("//localhost:3000");
-// let socket = io("https://chatserveracademlo.herokuapp.com/")
+let socket = io("https://chatserveracademlo.herokuapp.com/")
 
 export default socket;
